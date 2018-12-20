ce785e46f69109b00160602a5beae9b5c1be60ba
saksham90 <sakshamsidh@gmail.com>
2018-12-20 19:22:17 +0530

    fhgfgfg

--- lib/Pages/preciousGemPages/manik.dart
+++ lib/Pages/preciousGemPages/manik.dart
@@ -81,26 +81,114 @@ class _ManikPageState extends State<ManikPage> {
   Widget bottomData(int x) {
     if (x == 1) {
       return Column(
-        children: <Widget>[Text("Text1")],
+        children: <Widget>[Text("At Gem Selections we have a range of Manik i.e., Manik from Rs. 525/- Rt to Rs. 31000/- Rt.\n
+<b>Ruby :</b> At Gem Selections we have a range of Ruby i.e., Manikya from Rs. 525/ Rt to Rs. 41000/ Rt.\n\n
+You can have a look at these collections and decide what you want as per your requirement and budget.\n\n
+What ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.
+\n\n
+<b><u>Healing power of Ruby</u></b>
+\n\n
+The Red cosmic rays of the Ruby are connected with Sun which in Astrology represents the Soul. It also has the ruler ship over the Bone System on which the power of the body rests. The Red Cosmic Rays of Ruby affects the concentration of the mind and thus makes the mind goal oriented making it more prone to success. People who lack concentration or have lesser understanding benefit a lot from Ruby if they have the right type of Aura.
+\n\n
+Ayurved, the ancient Indian scripture dealing with medicine, tells about a number of uses of Ruby Ash (Manik bhasma). It is said to prolong life and control the three vital elements Vays (air), Pitta (fire and Kapha (water). A number of diseases like Tuberculosis, Pains, Boils, Ulcers, Eye Troubles, Constipation, Effects of Poisons can be cured by it. The burning sensation in the stomach and the limbs can also be cured by it.
+\n\n
+Heart diseases, mental problems, problems of the bones, headaches, loss of glow in skin, loss of appetite and indigestion can also be cured by the Red Cosmic Rays of the Ruby.
+\n\n
+The basic chakra or the mooldhara chakra contains a lot of Red energy in all living beings especially humans. The quantity of red colour determines the energy level of the person. If the basic chakra is depleted of the red energy due to any reason then the person has number of problems – like weakness, lack of heat in the body, circulation of blood is disturbed, the healing of injuries take a longer time to get alright and the body is unable to fight the attacks from bacteria and the person gets sick again and again.
+\n\n
+All the major chakras, when seen Clairevoyantly, have some red colour in them. Thus, the wearing of Ruby which transfers a lot of red cosmic energy into our system helps strengthening all the major chakras of the body.Where bones are brittle or fractures are not healing this can be an effective remedy.
+\n\n
+Astrological Relevance of the Ruby Gemstone Ruby Gemstone is the Gem of Star Sun and is used to empower it. It is has some general benefits which include:
+        \n\t
+1. It improves the real power of concentration. Better concentration of mind leads to all round achievements in personal and professional life.
+\n\t2. It improves the natural process of RBC building and helps in fighting anemia and other blood related deseases. It gives a glow on skin and face.
+\n\t3. It is the karka of Skeleton System and thus helps strengthening the Bones, Joints and the ligaments.
+\n\t4. It is the Gem of Sun, the most powerful body in our solar system, thus it helps achieve power, fame, glory and status.
+\n\t
+5. It gives emotional strength to the person to withstand deceit. It helps in strengthening the person\'s Bhagya i.e., Fate in people born with Leo i.e., Shimha Lagna, Aries i.e., Mesh Lagna, and Sagittarius i.e., Dhanu Lagna. It is also strongly recommended for people born in Libra i.e., Tula Lagna as the Sun God is the lord of the eleventh house - the house of all round gains. Let us understand the Effect of Ruby Gemstone on various Nakshatras:
+\n\n\n\t
+1. Ashwani: Gives bright eyes and sparkle.\n\t
+2. Bharni: Inproves initiative\n\t
+3. Kritika: Sharp features\n\t
+4. Rohini: Happy domestic life.\n\t
+5. Mraga: Diginity\n\t
+6. Ardra: Wealthy\n\t
+7. Punarvasu: Magnetic personality\n\t
+8. Pushya: Honorable and upright.\n\t
+9. Ashlesha: Wise and beautiful.\n\t
+10. Magha: Fearless\n\t
+11. Purva: Straightforward and successful\n\t
+12. Uttara: Money and Love\n\t
+13. Hasta: Earns respect and wealth.\n\t
+14. Chitra: Gets unearned income.\n\t
+15. Swati: Success in business.\n\t
+16. Vishaka: Kind and loveable\n\t
+17. Anuradha: Good talker. Passionate but passive nature.\n\t
+18. Jestha: Deep thinking and professional success.\n\t
+19. Mula: Healthy and strong\n\t
+20. Purvashada: Wealth and pleasures\n\t
+21. Uttarashada: Expert in conversations.\n\t
+22. Shravan: Kind hearted and modest.\n\t
+23. Dhanishta: Great satisfaction\n\t
+24. Shatataraka: Careful and wise.\n\t
+25. Purvabhadrapada: Mystic and Occult powers\n\t
+26. Uttarabhadrapada: Money and fixed deposits.\n\t
+27. Revati: Great savings in money.
+\n\n
+On this earth the most important is Sun, when it rises the day is created and when it goes down there is night. Its heat and light makes life on earth possible. it is the source of all energy and power. Sun and Ruby Gemstone are most important.")],
       );
     }
     if (x == 2) {
       return Column(
         children: <Widget>[
           Image.network(
-              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fnatural-pukhraj-stone-certified.jpg?alt=media"),
-          Text("Text2")
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2FIndian-Ruby.jpg?alt=media"),
+          Text("<b><u>INDIAN RUBY</u></b>\n\n
+At Gem Selections we have a range of Indian Ruby i.e., Manikya from Rs. 525/ Rt to Rs. 31000/ Rt.
+\n\n
+You can have a look at these collections and decide what you want as per your requirement and budget. \n\n
+What ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.
+\n\n
+This is the gem of Sun , it improves the concentration of mind and gives lusture to the skin . This gem gives all round success in life and is the karka for leadership. This improves the leadership qualities and puts the person in forefront . It gives intellectual capabilities and improves one’s writing and speaking powers.")
         ],
       );
     }
     if (x == 3) {
       return Column(
-        children: <Widget>[Text("Text3")],
+        children: <Widget>[
+          Image.network("https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fafrican-ruby.jpg?alt=media"),
+          Text("At Gem Selections we have a range of African Ruby i.e., Manikya from Rs. 525/ Rt to Rs. 31000/ Rt.
+\n\n
+You can have a look at these collections and decide what you want as per your requirement and budget. \n\n
+What ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.
+\n\n
+This is the gem of Sun , it improves the concentration of mind and gives lusture to the skin . This gem gives all round success in life and is the karka for leadership. This improves the leadership qualities and puts the person in forefront . It gives intellectual capabilities and improves one’s writing and speaking powers.")],
       );
     }
     if (x == 4) {
       return Column(
-        children: <Widget>[Text("Text4")],
+        children: <Widget>[
+          Image.network("https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fnew-burma-ruby.jpg?alt=media"),
+          Text(" <b><u>NEW BURMESE RUBY</u></b>\n\n
+At Gem Selections we have a range of new burmese Ruby i.e., Manikya from Rs. 525/ Rt to Rs. 31000/ Rt.
+\n\n
+You can have a look at these collections and decide what you want as per your requirement and budget. \n\n
+What ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.
+\n\n
+This is the gem of Sun , it improves the concentration of mind and gives lusture to the skin . This gem gives all round success in life and is the karka for leadership. This improves the leadership qualities and puts the person in forefront . It gives intellectual capabilities and improves one’s writing and speaking powers.")],
+      );
+    }
+     if (x == 5) {
+      return Column(
+        children: <Widget>[
+          Image.network("https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fold-burma-ruby.jpg?alt=media"),
+          Text("     <b><u>OLD BURMESE RUBY</u></b>\n\n
+At Gem Selections we have a range of old burmese Ruby i.e., Manikya from Rs. 525/ Rt to Rs. 31000/ Rt.
+\n\n
+You can have a look at these collections and decide what you want as per your requirement and budget. \n\n
+What ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.
+\n\n
+This is the gem of Sun , it improves the concentration of mind and gives lusture to the skin . This gem gives all round success in life and is the karka for leadership. This improves the leadership qualities and puts the person in forefront . It gives intellectual capabilities and improves one’s writing and speaking powers.")],
       );
     }
     return Container();
@@ -115,9 +203,9 @@ class _ManikPageState extends State<ManikPage> {
       body: ListView(
         children: <Widget>[
           Image.network(
-              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fnatural-pukhraj-stone-certified.jpg?alt=media"),
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fabout-ruby-gemstone.jpg?alt=media"),
           Text(
-            "Baingani Manik",
+            "MANIK STONE|RUBY",
             style: TextStyle(color: Colors.purple, fontSize: 20.0),
           ),
           Image.network(
