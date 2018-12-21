89fbf9cb1d85bcb98bd6e5f5a9d34d9b8d631218
saksham <sakshamsidh@gmail.com>
2018-12-21 17:20:40 +0530

    ynnhghng

--- lib/Pages/preciousGemPages/lehsunia.dart
+++ lib/Pages/preciousGemPages/lehsunia.dart
@@ -81,26 +81,48 @@ class _LehsuniaPageState extends State<LehsuniaPage> {
   Widget bottomData(int x) {
     if (x == 1) {
       return Column(
-        children: <Widget>[Text("Text1")],
+        children: <Widget>[Text("Lehsunia : At Gem Selections we have a range of Cats Eye i.e., Lehsunia from Rs. 50/ Rt to Rs. 3100/ Rt.\n\nYou can have a look at these collections and decide what you want as per your requirement and budget.\n\nWhat ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.\n\nThis is a Gem of Ketu (Dragon\’s Tail ) .By wearing this gem one is able to subdue his enemies and is able to remove the effect of any negative forces working on him. This gives one social recognition and sudden spurts of growth. It protects from Erratic income and bestows stability in life. This is the gem of Moksha and frees one from the cycles of life and death.")],
       );
     }
     if (x == 2) {
       return Column(
         children: <Widget>[
           Image.network(
-              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fnatural-pukhraj-stone-certified.jpg?alt=media"),
-          Text("Text2")
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fchrysoberyl-cats-eye.jpg?alt=media"),
+          Text("CHRYSOBERYL CATS EYE: This is the most coveted variety of Cat\'s Eye. These Gemstones have a sharply defined and slightly iridesecent ray or eye effect as it is known.\n\nThe Gemstones found in yellow or greenish - brown colour are the best in looks and are highly priced like all cats eyes, the ray appears at its best under a single sight or in direct sunlight. Multiple or differed illuminations spouls the effect.\n\nPrice : Rs. 1100/Rt to Rs. 5100/Rt.\n\nThis is a Gem of Ketu (Dragon\’s Tail ) .By wearing this gem one is able to subdue his enemies and is able to remove the effect of any negative forces working on him. This gives one social recognition and sudden spurts of growth. It protects from Erratic income and bestows stability in life. This is the gem of Moksha and frees one from the cycles of life and death.")
         ],
       );
     }
     if (x == 3) {
       return Column(
-        children: <Widget>[Text("Text3")],
+        children: <Widget>[
+          Image.network(
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fquartz-cats-eye.jpg?alt=media"),
+          Text("Quartz Cats Eye : At Gem Selections we have a range of Quartz Cats Eye i.e., Lehsunia from Rs. 50/ Rt to Rs. 3100/ Rt.\n\nYou can have a look at these collections and decide what you want as per your requirement and budget.\n\nWhat ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.\n\nThis is a Gem of Ketu (Dragon\’s Tail ) .By wearing this gem one is able to subdue his enemies and is able to remove the effect of any negative forces working on him. This gives one social recognition and sudden spurts of growth. It protects from Erratic income and bestows stability in life. This is the gem of Moksha and frees one from the cycles of life and death.")],
       );
     }
     if (x == 4) {
       return Column(
-        children: <Widget>[Text("Text4")],
+        children: <Widget>[
+          Image.network(
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fapatite-cats-eye.jpg?alt=media"),
+          Text("Apatite Cats Eye\n\nYou can have a look at these collections and decide what you want as per your requirement and budget.\n\nWhat ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.\n\nThis is a Gem of Ketu (Dragon\’s Tail ) .By wearing this gem one is able to subdue his enemies and is able to remove the effect of any negative forces working on him. This gives one social recognition and sudden spurts of growth. It protects from Erratic income and bestows stability in life. This is the gem of Moksha and frees one from the cycles of life and death.")],
+      );
+    }
+     if (x == 5) {
+      return Column(
+        children: <Widget>[
+          Image.network(
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fsillimanite-cats-eye.jpg?alt=media"),
+          Text("Sillimanite Cats Eye\n\nYou can have a look at these collections and decide what you want as per your requirement and budget.\n\nWhat ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.\n\nThis is a Gem of Ketu (Dragon\’s Tail ) .By wearing this gem one is able to subdue his enemies and is able to remove the effect of any negative forces working on him. This gives one social recognition and sudden spurts of growth. It protects from Erratic income and bestows stability in life. This is the gem of Moksha and frees one from the cycles of life and death.")],
+      );
+    }
+     if (x == 6) {
+      return Column(
+        children: <Widget>[
+          Image.network(
+              "https://firebasestorage.googleapis.com/v0/b/gemselections-add52.appspot.com/o/AppData%2Fprehnite-cats-eye.jpg?alt=media"),
+          Text("Prehnite Cats Eye :\n\nYou can have a look at these collections and decide what you want as per your requirement and budget.\n\nWhat ever you buy comes with a Lab Certificate, bill and Guarantee of purity and originality from our company and its promoter Mr. Pankaj Khanna.\n\nThis is a Gem of Ketu (Dragon\’s Tail ) .By wearing this gem one is able to subdue his enemies and is able to remove the effect of any negative forces working on him. This gives one social recognition and sudden spurts of growth. It protects from Erratic income and bestows stability in life. This is the gem of Moksha and frees one from the cycles of life and death.")],
       );
     }
     return Container();
@@ -110,7 +132,7 @@ class _LehsuniaPageState extends State<LehsuniaPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Purple Sapphire"),
+        title: Text("Cat's Eye"),
       ),
       body: ListView(
         children: <Widget>[
